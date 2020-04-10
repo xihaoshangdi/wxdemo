2f3913bbb254e6c818f90ceb90dc79a08e5805fc
xihaoshangdi <xihaoshangdi@163.com>
2020-04-10 11:58:11 +0800

    完成user部分

---
+++ src/components/BaseUserCard.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="card-area">
+        <BaseUserImg class="img"/>
+        <div class="card-info">
+            <div class="title">这是一个长的群名描述</div>
+            <div class="describe">这是最新的一条信息描述</div>
+        </div>
+        <div class="card-after">
+            <span>昨天</span>
+            <Icon name="zhong" class="card-icon"/>
+        </div>
+    </div>
+</template>
+
+
+<script lang="js">
+    import BaseUserImg from "@/components/BaseUserImg";
+    import Icon from "@/components/Icon";
+    export default {
+        name: "BaseUser",
+        components: {Icon, BaseUserImg}
+    };
+</script>
+
+<style lang="scss" scoped>
+    %ellipsis{
+        white-space:nowrap;
+        overflow:hidden;
+        text-overflow:ellipsis;
+    }
+    .card-area {
+        height: 54px;
+        display: flex;
+        align-items: center;
+        cursor: default;
+        font-size: 13px;
+        position: relative;
+        box-sizing: content-box;
+        padding: 3px 0;
+    }
+    .card-area:hover{
+        background-color: rgba(0,0,0,0.1);
+    }
+    .img {
+        width: 41px;
+        height: 41px;
+        margin: 0 10px;
+    }
+    .card-info{
+        width: 130px;
+        white-space:nowrap;
+        overflow:hidden;
+        text-overflow:ellipsis;
+    }
+    .title{
+        padding: 1px;
+        @extend %ellipsis
+    }
+    .describe{
+        padding: 1px;
+        color: #999999;
+        @extend %ellipsis
+    }
+    .card-after{
+        position: absolute;
+        right: 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        font-size: 12px;
+        color: #999999;
+    }
+    .card-icon{
+        margin-top: 5px;
+    }
+
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,74 +1,78 @@
 <template>
-  <Layout>
-    <div class="user-area">
-      <div class="nav">search</div>
-      <div class="user">
-        <div class="img" @click="xx"></div>
-        <span>xxxxxxx</span>
-      </div>
-    </div>
-    <div class="talk-area">
-      <div class="nav">
-        <frame-unit/>
-      </div>
-      用户对话页
-      <TheBaseInputArea/>
-    </div>
-  </Layout>
+    <Layout>
+        <div class="user-area">
+            <div class="nav">
+                <TheBaseInput/>
+                <Icon name="i-add" class="add-btn"/>
+            </div>
+
+            <BaseUser/>
+          <BaseUser/><BaseUser/><BaseUser/><BaseUser/><BaseUser/>
+
+
+        </div>
+        <div class="talk-area">
+            <div class="xx">
+                <frame-unit/>
+            </div>
+            用户对话页
+            <TheBaseInputArea/>
+        </div>
+    </Layout>
 </template>
 
 <script>
-// @ is an alias to /src
-import Layout from "../components/Layout";
-import TheBaseInputArea from "@/components/TheBaseInputArea";
-import FrameUnit from "@/components/FrameUnit";
+    // @ is an alias to /src
+    import Layout from "../components/Layout";
+    import TheBaseInputArea from "@/components/TheBaseInputArea";
+    import FrameUnit from "@/components/FrameUnit";
+    import TheBaseInput from "@/components/TheBaseInput";
+    import Icon from "@/components/Icon";
+    import BaseUser from "@/components/BaseUserCard";
 
-export default {
-  name: 'Home',
-  components: {
-    FrameUnit,
-    TheBaseInputArea,
-    Layout,
-  },
-  methods:{
-    xx(){
-      window.minimize();
-    }
-  }
-}
+    export default {
+        name: "Home",
+        components: {
+            BaseUser,
+            Icon,
+            TheBaseInput,
+            FrameUnit,
+            TheBaseInputArea,
+            Layout
+        },
+        methods: {}
+    };
 </script>
 
 <style lang="scss" scoped>
-  .user-area{
-    width: 200px;
-    border: 1px solid red;
-  }
-  .nav{
-    height: 50px;
-    border: 1px solid red;
-  }
-  .user{
-    height: 54px;
-    border: 1px solid red;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-  }
-  .img{
-    position: absolute;
-    width: 35px;
-    height: 35px;
-    left: 10px;
-    background-color: rgba(0,0,0,0.3);
-  }
+    .user-area {
+        width: 250px;
+        background-color: #E7E6E5;
+    }
+
+    .nav {
+        height: 60px;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        background-color: #DBD9D8;
+
+        .add-btn {
+            width: 1em;
+            height: 1em;
+            border-radius: 3px;
+            box-sizing: content-box;
+            padding: 4px;
+            background-color: rgba(0, 0, 0, 0.1);
+        }
+    }
 
 
-  .talk-area{
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    flex-grow: 1;
-  }
+    .talk-area {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        flex-grow: 1;
+    }
 
 </style>
