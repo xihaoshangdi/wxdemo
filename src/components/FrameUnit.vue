05d8ae2ad56cd797fe3c546e91bd6d9901a66f46
xihaoshangdi <xihaoshangdi@163.com>
2020-04-13 12:11:52 +0800

    修复更新了四个基本功能的bug

--- src/components/FrameUnit.vue
+++ src/components/FrameUnit.vue
@@ -2,14 +2,14 @@
     <div class="op-area">
         <Icon class="op-icon " @click.native="signTop" name="sign"/>
         <Icon class="op-icon " @click.native="win.minimize()" name="minus"/>
-        <Icon class="op-icon " @click.native="win.maximize()" name="max"/>
+        <Icon class="op-icon " @click.native="changeWindow" :name="[isMax!==true?'max':'max-after']"/>
         <Icon class="op-icon remove" @click.native="win.close()" name="remove"/>
     </div>
 </template>
 
 
 <script lang="js">
-    import Icon from "@/components/Icon";
+    import Icon from "@/components/BaseComponents/Icon";
 
     const {remote} = require("electron");
     const win = remote.getCurrentWindow();
@@ -18,6 +18,8 @@
         data() {
             return {
                 isTop:false,
+                isMax:false,
+                windowSize:undefined,
                 win:win
             };
         },
@@ -26,6 +28,19 @@
             signTop(){
                 this.isTop=!this.isTop;
                 win.setAlwaysOnTop(this.isTop);
+
+            },
+            changeWindow(){
+                if (this.isMax){
+                    win.setSize(this.windowSize[0],this.windowSize[1]);
+                    win.center();
+
+                }else{
+                    this.windowSize=win.getSize();
+                    console.log(this.windowSize);
+                    win.maximize();
+                }
+                this.isMax=!this.isMax;
             }
         }
     };
@@ -37,21 +52,18 @@
         justify-content: flex-end;
         align-items: center;
     }
-
     .op-icon {
         box-sizing: content-box;
-        width: 0.8em;
-        height: 0.8em;
+        width: 0.6em;
+        height: 0.6em;
         padding: 5px 10px;
-        color: black;
+        color: #676767;
         cursor: pointer;
     }
-
     .op-icon:hover {
         background-color: rgba(0, 0, 0, 0.1);
     }
-
     .remove:hover {
-        background-color: red;
+        background-color: #F45454;
     }
 </style>
